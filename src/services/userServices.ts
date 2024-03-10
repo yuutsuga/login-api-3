f976b09891b89a32ac538a7a67e2f131428b1831
Storm <korudoarashi@gmail.com>
2024-03-10 06:41:24 -0300

    [CORE]: Added comments to where input code

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -16,18 +16,28 @@ type User = {
 
 export const userService = {
   create: (user: UserCreateInput): User | null => {
+    /* Faça seu código de criação de usuário*/
+
     return null;
   },
   get: (): Array<User> => {
+    /* Faça seu código de retorno de usuários*/
+
     return []
   },
   find: (id: number): User | null => {
+    /* Faça seu código de busca de usuário*/
+
     return null;
   },
   update: (id: number, user: UserUpdateInput): boolean => {
+    /* Faça seu código de edição de usuário*/
+
     return false;
   },
   delete: (id: number): boolean => {
+    /* Faça seu código de deleção de usuário*/
+
     return false;
   }
 }
