cbea79db37eb3091a6c1065a7f09bfc9bfb1f69f
Gabriel Silveira <gabrielfagundessilveira@gmail.com>
2024-03-18 14:41:11 -0300

    [FIX]: CRUD created in userServices.ts

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,3 +1,5 @@
+import prisma from "../database";
+
 type UserCreateInput = {
   name: string,
   username: string
@@ -17,27 +19,47 @@ type User = {
 export const userService = {
   create: async (user: UserCreateInput): Promise<User | null> => {
     /* Faça seu código de criação de usuário*/
+    const newUser = await prisma.user.create({
+      data: user
+    });
 
-    return null;
+    return newUser;
   },
   get: async (): Promise<Array<User>> => {
     /* Faça seu código de retorno de usuários*/
+    const usersInfo = await prisma.user.findMany({ });
 
-    return []
+    return usersInfo;
   },
   find: async (id: number): Promise<User | null> => {
     /* Faça seu código de busca de usuário*/
+    const userInfoById = await prisma.user.findFirst({
+      where: {
+        id
+      }
+    });
 
-    return null;
+    return userInfoById;
   },
   update: async (id: number, user: UserUpdateInput): Promise<boolean> => {
     /* Faça seu código de edição de usuário*/
+    const userUpdated = await prisma.user.updateMany({
+      where: {
+        id
+      },
+      data: user
+    });
 
-    return false;
+    return userUpdated.count > 0;
   },
   delete: async (id: number): Promise<boolean> => {
     /* Faça seu código de deleção de usuário*/
+    const userDeleted = await prisma.user.deleteMany({
+      where: {
+        id
+      }
+    });
 
-    return false;
+    return userDeleted.count > 0;
   }
 }
